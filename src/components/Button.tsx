a420cbc72145d5f000b43fa126e52307a6a55ebf
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-04-18 18:13:08 +0200

    Update Button Types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,19 @@
 import './Button.css'
 
-
+/*
+    text: text of the button
+    buttonType:
+        prepare
+        work
+        rest
+        repetitions
+        accept
+    buttonValue: used by timer settings only. Number type
+*/
 export interface ButtonProps {
     text: string;
+    buttonType: string;
+    buttonValue: number;
 }
 
 export function Button(properties: ButtonProps) {
